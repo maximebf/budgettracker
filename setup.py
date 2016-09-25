a39b858afe29cc35bcb7ec837077769c8e043d28
maximebf <maxime.bouroumeau@gmail.com>
2016-09-25 23:52:20 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='budgettracker',
+    version='0.1',
+    url='http://github.com/maximebf/budgettracker',
+    license='MIT',
+    author='Maxime Bouroumeau-Fuseau',
+    author_email='maxime.bouroumeau@gmail.com',
+    description='Simple budget tracking app',
+    packages=find_packages(),
+    package_data={
+        'budgettracker': ['web/templates/*.html'],
+    },
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'unicodecsv',
+        'flask',
+        'monthdelta'
+    ],
+    entry_points='''
+        [console_scripts]
+        budgettracker=budgettracker
+        budgettracker-web=budgettracker.web
+    '''
+)
